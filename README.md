c43d1888d164ef375b2b4aa2af1531a2751301b2
Caglar Kullu <ckullu@mifinity.com>
2025-05-27 16:27:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# afs_mock_project
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/afs_mock_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.afs_mock_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
