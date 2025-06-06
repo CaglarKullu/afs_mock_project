ba45cf8d95303caf6596c9666fe52bbc8f2f2993
Caglar Kullu <caglarkullu@gmail.com>
2025-06-06 17:46:34 +0100

    main entry changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:afs_mock_project/common/index.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'features/jobs/index.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+
+    return MaterialApp(
+      title: 'AFS Jobs',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: AppScrollBehavior(),
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: themeMode,
+      home: const HomeScreen(),
     );
   }
 }
