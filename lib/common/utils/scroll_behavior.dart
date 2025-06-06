71514bfcd9725eee56425ed866dc72a42e614bb0
Caglar Kullu <caglarkullu@gmail.com>
2025-06-06 17:45:08 +0100

    add draggable scrolle behevior

---
+++ lib/common/utils/scroll_behavior.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+/// Custom scroll behavior to support touch, mouse, trackpad, and stylus devices.
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.unknown,
+      };
+}
